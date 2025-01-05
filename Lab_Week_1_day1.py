1e200a5845819232974638bf98869e2bda74e627
Satta Deepan Sahu <sdsahu98@gmail.com>
2025-01-05 23:48:39 +0530

    Update Lab_Week_1_day1.py

--- Lab_Week_1_day1.py
+++ Lab_Week_1_day1.py
@@ -4,8 +4,17 @@ Problem 1 :
  Write a program to take the diamater of a circle as user input
  and calculate and print its area & circumference.'''
 
+# input diameter of the circle.
 diameter = float(input("Enter the Diameter: "))
+
+# calculate the area of the circle.
 area = 3.14*pow(diameter/2,2)
+
+# calculate the circumference of the circle.
 circumference = 2*3.14*(diameter/2)
+
+# print the area of the circle.
 print("Area: ", round(area, 2))
+
+# print the circumference of the circle rounded upto 2 decimal places.
 print("Circumference: ", round(circumference, 2))
