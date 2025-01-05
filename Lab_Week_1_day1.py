efaa2c0a6f87350105eff5820e5b5a07a3147ede
Satta Deepan Sahu <sdsahu98@gmail.com>
2025-01-05 23:28:31 +0530

    Update Lab_Week_1_day1.py
    Changed input type from int to float and used round() for outputs to restrict decimals upto 2 places in output

--- Lab_Week_1_day1.py
+++ Lab_Week_1_day1.py
@@ -4,8 +4,8 @@ Problem 1 :
  Write a program to take the diamater of a circle as user input
  and calculate and print its area & circumference.'''
 
-diameter = int(input("Enter the Diameter: "))
+diameter = float(input("Enter the Diameter: "))
 area = 3.14*pow(diameter/2,2)
 circumference = 2*3.14*(diameter/2)
-print("Area: ",area)
-print("Circumference: ",circumference)
+print("Area: ", round(area, 2))
+print("Circumference: ", round(circumference, 2))
